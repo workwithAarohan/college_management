a005beb41c4f2c6e32fd47b57ec363597e43256c
chapagainsuman427 <chapagainsuman427@gmail.com>
2021-07-12 10:51:28 +0545

    Task Finished

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index()
+    {
+        return 'Mero ta kaam sakiyo kta ho!! Ma sutey, goodnight!!';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,5 +21,11 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
+
+Route::get('/user', function () {
+    return view('user');
+});
+
+Route::get('/home',[HomeController::class,'index'])->name('home.index');
 
 require __DIR__.'/auth.php';
