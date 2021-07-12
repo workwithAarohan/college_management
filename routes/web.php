635ea53b412689904fbc00cd517010bd87739ba0
sujan210 <sujankunwar2021@gmail.com>
2021-07-12 10:46:22 +0545

    new html file

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/dashboard', function () {
 })->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
+
+Route::get('/create',function(){
+    return view('create');
+});
